cab989e7694c54183cbdfa7efe96a22f5f548870
felipeschawsnekker <felipesilvamoura2024@gmail.com>
2025-05-21 19:37:17 -0300

    Create matrizes.cpp

---
+++ matrizes.cpp
@@ -0,0 +1,30 @@
+using namespace std;
+#include <iostream>
+
+int main(){
+int a;
+int b;
+float y[2][2];
+
+for (a=0; a<2; a++){
+  for(b=0;b<2; b++){
+    cout<<"Digite um número: ";
+    cin>>y[a][b];
+    
+    
+}
+}
+
+for (a=0; a<2; a++){
+    
+  for(b=0;b<2; b++){
+      cout<<y[a][b]<<" ";
+  }
+  cout<<endl;
+}
+
+
+
+
+return 0;
+}
